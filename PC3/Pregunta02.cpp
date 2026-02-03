078a3880f9dea94d0bdf3c4a2dd480f66eae9140
Daniel <daniel.diaz.m@uni.pe>
2026-02-02 23:30:25 -0500

    PC3

--- PC3/Pregunta02.cpp
+++ PC3/Pregunta02.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
 using namespace std;
 
-/*void Apunta(char (*p)[], char cadena[]){
-    if(**p =! '\0'){
-        if(( *p == cadena && **p != ' ') ||  ){
+bool esVocal(char c){ // Función para verificar si un caracter es una vocal
+    if (c >= 'A' && c <= 'Z'){
+        c = c + 32; // De mayuscula a minuscula
+    } 
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'); // Retorna 1 si se cumple una de las cinco condiciones, sino se cumple ninguna retorna 0
+}
 
+int contarVocalesInicio(char* cadena){
+    char* p = cadena; // Puntero a caracter se iguala a la referencia de un caracter
+    int contador = 0; // Contador de palabras que comienzan en vocal
+    bool nuevaPalabra = true; // Para saber si estamos al inicio de una palabra(true) caso contrario(false)
+
+    while (*p != '\0') {
+        if (*p != ' '){ // Se ejecuta si no estamos en un espacio
+            if (nuevaPalabra){ 
+                if (esVocal(*p)){ // Verificamos si el caracter es un vocal o no
+                    contador++;
+                }
+                nuevaPalabra = false; // Como ya estamos en la palabra no contamos el resto de las letras
+            }
+        } 
+        else{
+            nuevaPalabra = true; // Si encontramos un espacio sabemos, seguimos avanzando hasta encontrar el inicio de una palabra
         }
+        p++; // Para ir avanzando en la palabra
     }
+    return contador;// Imprimimos cuantas palabras encontramos
+}
 
-}*/
-
-int main(){
+int main() {
     char cadena[] = "     Este es    un  ejemplo en C++   END";
-    /*char (*p)[] = &cadena;*/
-    cout<<cadena<<endl;
+    
+    cout << "texto: " << cadena << endl;
+    
+    int resultado = contarVocalesInicio(cadena);
+    
+    cout << "Numero de palabras que comienzan en vocal: " << resultado << endl;
+
+    return 0;
 }
