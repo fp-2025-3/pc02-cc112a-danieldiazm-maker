516310d76f1a71298ea2e4b5c255164ecf29a9bc
Eloy <daniel.diaz.m@uni.pe>
2026-02-04 17:17:12 -0500

    Lab4

---
+++ Semana4/Lab4/Ejercicio4.cpp
@@ -0,0 +1,18 @@
+/*
+Ejercicio 4 (recursividad)
+
+Dado un arreglo unidimensional A de n (máximo 15) números enteros positivos. Se desea dividir el arreglo en segmentos contiguos, cumpliendo las siguientes condiciones:
+
+Cada segmento debe contener al menos un elemento.
+
+El valor de un segmento se define como el producto de todos sus elementos.
+
+El costo total de una segmentación es la suma de los valores de todos los segmentos.
+
+Implementar una función recursiva para determinar el costo mínimo posible que se puede obtener al segmentar el arreglo.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    return 0;
+}
