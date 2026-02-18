02c51207dfeab822a7519feae2d9eed7ae6c7045
Daniel <daniel.diaz.m@uni.pe>
2026-02-18 16:02:06 -0500

    Semana 6 Teoria y ejericio1

---
+++ Semana6/LAB6/Ejercicio01.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream> 
+
+using namespace std;
+
+const int DIAS = 7;
+
+int main() {
+    ofstream archivo("ventas.txt");
+
+    if (!archivo) {
+        cout << "Error al abrir el archivo";
+        return 1;
+    }
+
+    double total = 0.0;
+    double mayorVenta;
+    double menorVenta;
+    double venta;
+
+    for(int i = 1; i <= DIAS; ++i){
+        cout << "Ingrese la venta del dia " << i << ": ";
+        cin >> venta;
+
+        archivo << venta << endl;
+        
+        total += venta;
+
+        if(i == 1){
+            mayorVenta = venta;
+            menorVenta = venta;
+        }
+        else{
+            if(venta > mayorVenta){
+                mayorVenta = venta;
+            }
+            if(venta < menorVenta){
+                menorVenta = venta;
+            }
+        }
+    }
+
+    double promedio = total / DIAS;
+
+    archivo << "-------------------------" << endl;
+    archivo << "RESUMEN" << endl;
+    archivo << "Total: " << total << endl;
+    archivo << "Promedio: " << promedio << endl;
+    archivo << "Mayor venta: " << mayorVenta << endl;
+    archivo << "Menor venta: " << menorVenta << endl;
+
+    archivo.close();
+
+    cout << "Resumen generado con exito en ventas.txt" << endl;
+
+    return 0;
+}
