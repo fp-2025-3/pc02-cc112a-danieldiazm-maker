05cbc7d2b64ff60538966eb6c49288d7a0c6db48
Daniel <daniel.diaz.m@uni.pe>
2026-03-03 00:32:59 -0500

    Mejora PC5 Pregunta02

--- Semana7/PC5/Mejora_Examen/Pregunta02.cpp
+++ Semana7/PC5/Mejora_Examen/Pregunta02.cpp
@@ -3,17 +3,17 @@
 
 using namespace std;
 
-struct Empleado {
+struct Empleado{
     int id; // Comienza en 1
     char nombre[30];
     double salario;
 };
 
-int main() {
+int main(){
     fstream archivo("input/empleados.dat", ios::in | ios::out | ios::binary); // Recordar: ios::in (lectura), ios::out (escritura), ios::binary (modo binario)
 
-    if (!archivo) {
-        cout << "Error: No se pudo abrir el archivo input/empleados.dat" << endl;
+    if (!archivo){
+        cout << "Error: No se pudo abrir el archivo" << endl;
         return 1;
     }
 
@@ -21,36 +21,30 @@ int main() {
     cout << "Ingrese ID de empleado: ";
     cin >> ID;
 
-    // 1. CÁLCULO DE LA POSICIÓN (Acceso Directo)
-    // El ID 1 está en el byte 0, el ID 2 en el byte 1 * sizeof(Empleado), etc.
-    streampos posicion = (ID - 1) * sizeof(Empleado);
+    long desplazamiento = (ID - 1) * sizeof(Empleado); // Desplazamiento que se dara
 
-    // 2. MOVER PUNTERO DE LECTURA (seekg)
-    archivo.seekg(posicion);
+    archivo.seekg(desplazamiento); // Lo buscamos para leer (seekg)
 
     Empleado E1;
-    // Intentamos leer el registro completo
-    if (archivo.read(reinterpret_cast<char*>(&E1), sizeof(Empleado))) {
+    if(archivo.read(reinterpret_cast<char*>(&E1), sizeof(Empleado))){ // Leemos usando la forma para binario
         
         cout << "\n--- DATOS DEL EMPLEADO ---" << endl;
         cout << "ID: " << E1.id << endl;
         cout << "Nombre: " << E1.nombre << endl;
         cout << "Salario: " << E1.salario << endl;
 
-        // 3. MODIFICACIÓN DEL SALARIO
         double nuevoSalario;
         cout << "\nIngrese nuevo salario: ";
-        cin >> nuevoSalario;
+        cin >> nuevoSalario; // Modificamos el salario
         E1.salario = nuevoSalario;
 
-        // 4. ACTUALIZACIÓN (Acceso Directo para escribir)
-        // Debemos mover el puntero de escritura (seekp) a la misma posición
-        archivo.seekp(posicion);
+        archivo.seekp(desplazamiento); // Volvemos para escribir (seekp)
         archivo.write(reinterpret_cast<char*>(&E1), sizeof(Empleado));
 
         cout << "\nSalario actualizado correctamente." << endl;
-    } else {
-        cout << "Error: El ID " << ID << " no existe o esta fuera de rango." << endl;
+    }
+    else{ // Caso contrario se imprime:
+        cout << "Error: El ID no existe." << endl;
     }
 
     archivo.close();
