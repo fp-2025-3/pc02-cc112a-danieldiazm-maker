666eed878460430740e432df8fd38ca44bdb56f6
Daniel <daniel.diaz.m@uni.pe>
2026-03-06 23:56:39 -0500

    Mejora Pregunta02.cpp EF

--- Semana8/EF/Mejora_Examen/Pregunta02.cpp
+++ Semana8/EF/Mejora_Examen/Pregunta02.cpp
@@ -5,34 +5,39 @@ using namespace std;
 struct Punto{
     double x;
     double y;
-    void leerPunto(Punto *p){
-        cout << "Digite el punto en x: ";
-        cin >> p -> x;
-        cout << "Digite el punto en y: ";
-        cin >> p -> y;
-    }
-    double distanciaOrigen(const Punto* p){
-        double D = 0;
-        D = sqrt(pow(p->x , 2) + pow(p->y , 2));
-        return D;
-    }
-    Punto* masLejano(Punto* p1, Punto* p2){
-        if(distanciaOrigen(p1) >= distanciaOrigen(p2)){
-            return p1;
-        }
-        else{
-            return p2;
-        }
-    }
 };
 
+void leerPunto(Punto *p){ // Correcion moviendo las funciones
+    cout << "Digite el punto en x: ";
+    cin >> p -> x;
+    cout << "Digite el punto en y: ";
+    cin >> p -> y;
+}
+double distanciaOrigen(const Punto* p){
+    double D = 0;
+    D = sqrt(pow(p->x , 2) + pow(p->y , 2));
+     return D;
+}
+Punto* masLejano(Punto* p1, Punto* p2){
+    if(distanciaOrigen(p1) >= distanciaOrigen(p2)){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+
 int main(){
-    Punto *P1;
-    Punto *P2;
+    Punto P1;
+    Punto P2;
     cout << "Leyendo P1: " << endl;
-    P1->leerPunto(P1);
+    leerPunto(&P1);
     cout << "Leyendo P2: " << endl;;
-    P1->leerPunto(P2);
-    cout << "Las coordenadas del puntero mas lejano son: " << endl << "x: " << P1->masLejano(P1,P2)->x << endl << "y: " << P1->masLejano(P1,P2)->y << endl;
+    leerPunto(&P2);
+    // Creamos una variable en donde guardamos el retorno para mejor lectura del codigo
+
+    Punto* lejano = masLejano(&P1,&P2);
+
+    cout << "Las coordenadas del puntero mas lejano son: " << endl << "x: " << lejano->x << endl << "y: " << lejano->y << endl;
     return 0;
 }
